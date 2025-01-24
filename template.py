09b9b5356f54395aa5e1c2aa4b2d83be8017ebc2
iamk3nt <kentrameez@gmail.com>
2025-01-24 07:11:53 +0530

    Added Logger

--- template.py
+++ template.py
@@ -40,6 +40,7 @@ list_of_files = [
     f"source/{PROJECT_NAME}/entity/__init__.py",
     f"source/{PROJECT_NAME}/entity/config_entity.py",
     f"source/{PROJECT_NAME}/constants/__init__.py",
+    f"source/{PROJECT_NAME}/logger.py",
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
